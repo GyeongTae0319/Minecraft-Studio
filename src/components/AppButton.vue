c925bffd09ec4cd7dc92f8f6bea4673748b62500
강경태 <gyeongtae020319@gmail.com>
2021-02-08 00:28:29 +0900

    기본 버튼 컴포넌트 제작
    - 키보드 탐색 감지 후 클래스 지정
    - TODO: to 속성으로 링크 지정 기능 필요

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="getClassList"><slot/></button>
+</template>
+
+<script lang="ts">
+import { ClassList } from "@/vue-global";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    computed: {
+        getClassList() {
+            return ["app-button", {
+                "keyboard-mode": this.$a11y.isKeyboardMode()
+            }] as ClassList;
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+    cursor: pointer;
+}
+</style>
