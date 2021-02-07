2501ef43c88a5c30bfc5b65576f91a9c7093d6a5
강경태 <gyeongtae020319@gmail.com>
2021-02-08 00:08:38 +0900

    전역 SCSS 파일 제작 및 적용
    - 전역 폰트, 믹스인, 변수 추가

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+                    @import "@/assets/styles/_variables.scss";
+                    @import "@/assets/styles/_fonts.scss";
+                    @import "@/assets/styles/_mixins.scss";
+                `,
+            }
+        }
+    }
+};
