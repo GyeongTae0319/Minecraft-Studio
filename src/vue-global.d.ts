64d3a8a4185b84884539408b6c67cf9e4767e1b5
강경태 <gyeongtae020319@gmail.com>
2021-02-08 00:06:24 +0900

    플러그인 타입 선언
    - 이전 커밋에서 누락됨 (i18n, a11y 플러그인 커밋)

---
+++ src/vue-global.d.ts
@@ -0,0 +1,45 @@
+import { App, ComponentCustomProperties, Ref } from "vue";
+
+// Data structure
+export declare type Dictionary<T> = { [key: string]: T };
+export declare type ClassList = String | Array<string | Dictionary<Boolean>> | Dictionary<Boolean>
+
+// * Plugins * //
+// I18n
+export declare interface I18n {
+    private locale: Ref<string>;
+    private collection: LanguageCollection;
+
+    private languageList: string[];
+
+    constructor(collection: LanguageCollection): void;
+
+    setLocale(locale: string): boolean;
+    getLocale(): string;
+    getLanguageList(): string[];
+
+    t(key: string): string;
+
+    install(vue: App): void;
+}
+// A11y
+export declare interface A11y {
+    private keyboardMode: Ref<Boolean>;
+
+    constructor(): void;
+
+    private onKeyDown(event: KeyboardEvent): void;
+    private onMouseMode(): void;
+
+    isKeyboardMode(): Boolean;
+
+    install(vue: App): void;
+}
+
+// Inject plugins
+declare module "@vue/runtime-core" {
+    export interface ComponentCustomProperties {
+        $i18n: I18n;
+        $a11y: A11y;
+    }
+}
